fcfd6c89cb96bf9f7748a98b804e055dbbe1f19d
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-26 19:43:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,4 +11,19 @@ const codes = [
   "a"
 ];
 
-
+function init(){
+  documentBody = document.body;
+  let index = 0;
+  documentBody.addEventListener('keydown', function(e) {
+    const key = e.which;
+    if (key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert("Congratulations!");
+        
+        index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
